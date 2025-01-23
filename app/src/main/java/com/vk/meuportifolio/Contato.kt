fae587cfa98685f0da0000b88fc919c6bcdc7e82
pablo <pablogonzaga14@gmail.com>
2025-01-23 15:57:04 -0300

    deixando a imagem circular, formatando os textos e adicionando navegação entre telas

---
+++ app/src/main/java/com/vk/meuportifolio/Contato.kt
@@ -0,0 +1,16 @@
+package com.vk.meuportifolio
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Contato : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_contato)
+
+    }
+}
--- app/src/main/java/com/vk/meuportifolio/MainActivity.kt
+++ app/src/main/java/com/vk/meuportifolio/MainActivity.kt
@@ -1,19 +1,38 @@
 package com.vk.meuportifolio
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.vk.meuportifolio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fotoPerfil = BitmapFactory.decodeResource(resources, R.drawable.foto_perfil2)
+        val circulo = RoundedBitmapDrawableFactory.create(resources, fotoPerfil);
+        circulo.isCircular = true
+        binding.imgFotoPerfil.setImageDrawable(circulo)
+
+        binding.btProjetos.setOnClickListener {
+            val intent = Intent (this, Projetos::class.java)
+            startActivity(intent)
         }
-    }
+        binding.btContato.setOnClickListener {
+            val intent = Intent (this, Contato::class.java)
+            startActivity(intent)
+        }
+        }
+
 }

45f9ebe5ce7dae3bbb59e5455cb0e917e0ade20a
pablo <pablogonzaga14@gmail.com>
2025-01-27 20:00:51 -0300

    Toolbar

--- app/src/main/java/com/vk/meuportifolio/Contato.kt
+++ app/src/main/java/com/vk/meuportifolio/Contato.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.vk.meuportifolio.databinding.ActivityContatoBinding
 
 class Contato : AppCompatActivity() {
+
+    private lateinit var binding: ActivityContatoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_contato)
+        binding = ActivityContatoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val toolbarContato = binding.toolbarContato
+        toolbarContato.setNavigationOnClickListener(){
+            finish()
+        }
     }
 }
